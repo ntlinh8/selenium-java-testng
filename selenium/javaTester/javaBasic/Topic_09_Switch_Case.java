1ff6bbc05cd641f7edd3a93e7570fe3658dc4ac5
Nguyen Thuy Linh <thuylinh2409nguyen@gmail.com>
2023-04-16 11:54:40 +0700

    Upload Topic_10_For_Loop_Statement

--- selenium/javaTester/javaBasic/Topic_09_Switch_Case.java
+++ selenium/javaTester/javaBasic/Topic_09_Switch_Case.java
@@ -81,42 +81,23 @@ public class Topic_09_Switch_Case {
 		int a = scanner.nextInt();
 		switch (a) {
 		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
 			System.out.println("The day number is 31");
 			break;
 		case 2:
 			System.out.println("The day number is 28");
 			break;
-		case 3:
-			System.out.println("The day number is 31");
-			break;
-		case 4:
-			System.out.println("The day number is 30");
-			break;
-		case 5:
-			System.out.println("The day number is 31");
-			break;
 		case 6:
-			System.out.println("The day number is 30");
-			break;
-		case 7:
-			System.out.println("The day number is 31");
-			break;
-		case 8:
-			System.out.println("The day number is 31");
-			break;
 		case 9:
-			System.out.println("The day number is 30");
-			break;
-		case 10:
-			System.out.println("The day number is 31");
-			break;
+		case 4:
 		case 11:
 			System.out.println("The day number is 30");
 			break;
-		case 12:
-			System.out.println("The day number is 31");
-			break;
-			
 		default:
 			System.out.println("Gia tri ko hop le");
 			break;
