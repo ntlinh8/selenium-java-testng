a8aa94115daa991df0927b0e35cc5517af15e92b
SBC\nt.linh8 <nt.linh8@samsung.com>
2023-04-17 07:30:00 +0700

    upload Topic_08_Condition_Statement

---
+++ selenium/javaTester/javaBasic/Topic_08_Condition_Statement.java
@@ -0,0 +1,16 @@
+package javaTester.javaBasic;
+
+public class Topic_08_Condition_Statement {
+	public static void main(String[] args) {
+		boolean status  = 5 > 3;
+		
+		//If condition statement requires a contion as argument
+		//if statement only checks one condition
+		//if condition is true, the code statement which is in the if statement, will running
+		if (status) {
+			System.out.println("Day la ben trong ham if");
+		}
+		
+	}
+
+}
